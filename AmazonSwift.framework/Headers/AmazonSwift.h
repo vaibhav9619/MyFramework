20325871c265d778e704461c2d68e760c971ebde
Vaibhav Sadana <vaibhav.sadana@netgear.com>
2020-10-01 17:10:35 +0530

    helo

---
+++ AmazonSwift.framework/Headers/AmazonSwift.h
@@ -0,0 +1,19 @@
+//
+//  AmazonSwift.h
+//  AmazonSwift
+//
+//  Created by Vaibhav Sadana on 23/09/20.
+//  Copyright © 2020 Vaibhav Sadana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AmazonSwift.
+FOUNDATION_EXPORT double AmazonSwiftVersionNumber;
+
+//! Project version string for AmazonSwift.
+FOUNDATION_EXPORT const unsigned char AmazonSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmazonSwift/PublicHeader.h>
+
+
